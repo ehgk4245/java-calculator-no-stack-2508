72d1d2fff4c056f927756e919f6049a18e74c085
ehgk4245 <ehgkrk14@naver.com>
2025-08-01 14:43:09 +0900

    step 1, 2, 3 / green

--- src/main/java/com/study/Calc.java
+++ src/main/java/com/study/Calc.java
@@ -2,6 +2,7 @@ package com.study;
 
 public class Calc {
     public static int run(String expression) {
-        return -1;
+        char[] charArray = expression.replaceAll("\\s+", "").toCharArray();
+        return Character.getNumericValue(charArray[0]) + Character.getNumericValue(charArray[2]);
     }
 }

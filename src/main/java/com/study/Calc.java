8c5cb478c72215b7166baa4d51922bfaf55a627d
ehgk4245 <ehgkrk14@naver.com>
2025-08-01 16:26:42 +0900

    step 5, 6, 7 / green

--- src/main/java/com/study/Calc.java
+++ src/main/java/com/study/Calc.java
@@ -1,13 +1,46 @@
 package com.study;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class Calc {
     public static int run(String expression) {
-        String[] numbers = expression.split("\\+", 2);
-        return Arrays.stream(numbers)
+        char operator = findOperator(expression);
+        String[] strNumbers = expression.split(Pattern.quote(String.valueOf(operator)), 2);
+        int[] numbers = Arrays.stream(strNumbers)
                 .map(String::strip)
                 .mapToInt(Integer::parseInt)
-                .sum();
+                .toArray();
+        return calculate(operator, numbers);
+    }
+
+    private static int calculate(char operator, int[] numbers) {
+        switch(operator) {
+            case '+' -> {
+                return plus(numbers[0], numbers[1]);
+            }
+            case '-' -> {
+                return minus(numbers[0], numbers[1]);
+            }
+            default -> {
+                return 0;
+            }
+        }
+    }
+
+    private static char findOperator(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '+' || c == '-') return c;
+        }
+        return 'x';
+    }
+
+    private static int plus(int a, int b) {
+        return a + b;
+    }
+
+    private static int minus(int a, int b) {
+        return a - b;
     }
 }
